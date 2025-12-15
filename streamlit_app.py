b7981e32c6de3b97efadcff0de7663fa18736f71
blm14 <blm14@columbia.edu>
2025-12-14 20:24:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,11 +29,6 @@ ingredients_list = st.multiselect(
 
 
 
-pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
 
 
 
