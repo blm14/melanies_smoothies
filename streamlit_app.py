aa38292a6fbf4e588c7a6dfceaca3c2d5452ea61
blm14 <blm14@columbia.edu>
2025-12-14 20:21:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,12 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:',name_on_order)
 
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe
+    
+)
+
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON"))
@@ -27,11 +33,7 @@ st.stop()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
-ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:',
-    my_dataframe
-    
-)
+
 
 if ingredients_list:
     #st.write(ingredients_list)
